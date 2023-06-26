2bcf366e2b5c0c669ee8d72774cd7e9ac79bb2d5
onissyuta <dev@pixelog.net>
2023-06-26 17:02:39 +0900

    6月26日に取り組んだチュートリアル

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-</script>
+import Form from './components/InputName.vue'
+import { ref } from 'vue'
 
+const count = ref(0)
+const dynamicId = ref("title")
+
+function increment() {
+  count.value++
+}
+
+
+const text = ref("")
+function onInput(e){
+  text.value = e.target.value
+}
+
+
+let text_flag = ref(false)
+function toggle(){
+  text_flag.value = !text_flag.value
+}
+
+</script>
+<!--
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
@@ -16,11 +38,39 @@ import TheWelcome from './components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+-->
+
+<template>
+  <p>
+    <div v-bind:class="dynamicId">Red</div>
+    <div :class="dynamicId">Red</div>
+  </p>
+
+  <p>
+    <button v-on:click="increment">count is : {{ count }}</button>
+  </p>
+
+
+  <input :value="text" @input="onInput">
+  <p>{{ text }}</p>
+
+
+
+  <p>
+    <button @click="toggle">切り替えボタン {{ text_flag }}</button>
+    <h1 v-if="text_flag">こんにちは</h1>
+  </p>
+
+  <Form />
+
+</template>
+
 
 <style scoped>
 header {
   line-height: 1.5;
 }
+
 
 .logo {
   display: block;
@@ -44,4 +94,13 @@ header {
     flex-wrap: wrap;
   }
 }
+
+button {
+
+}
+
+.title {
+  color: red;
+}
+
 </style>
---
+++ src/components/InputName.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const name = ref("")
+function onInput(e){
+    name.value = e.target.value
+
+}
+</script>
+
+<template>
+    名前: <input :text="name" @input="onInput">
+    <div>こんにちは、{{ name }}さん</div>
+</template>
+
+<style scoped>
+    body {
+        background: #fff;
+    }
+</style>
